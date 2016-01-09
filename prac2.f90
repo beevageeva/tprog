6e5bd6048c532c9fa586c353e9287f6f15611ead
beatrice <beatrice@localhost.localdomain>
2016-01-09 21:46:15 +0000

    prac2

--- prac2.f90
+++ prac2.f90
@@ -4,6 +4,8 @@ PROGRAM prac2
   INTEGER :: n,localStart,localEnd, k, maxValue
   INTEGER, dimension(:), allocatable :: arr
   INTEGER :: myRank, numProcs, status(MPI_STATUS_SIZE), ierr
+
+
   
   call MPI_INIT(ierr)
   call MPI_COMM_RANK(MPI_COMM_WORLD, myRank, ierr)
@@ -12,10 +14,19 @@ PROGRAM prac2
     print*, "Introduce n:"
     read*, n
   end if
+  ! send n from process master(0) to all processes
   call mpi_bcast(n, 1, mpi_integer, master, mpi_comm_world, ierr)
+	! the problem is equivalent to:
+	!for a given n find all prime numbers x
+	! n > x > sqrt(n)   
+	!and put them in a list(and print the max afterwards)
+  !(https://github.com/beevageeva/tprog/blob/master/prac2p.f90)
+  ! but here we create an initial list with consecutive numbers(from 2 to n) and eliminate
+  ! multiples of k for k = 2, sqrt(n)  
+  ! each process creates its own initial array 
   k = int((n-1)/numProcs)
   localStart = 2 + myRank * k
-  ! n might not ne a multiple of numProcs so the last process will have more
+  ! n - 1 might not ne a multiple of numProcs and the last process would have more
   ! elements than the rest
   if (myRank .eq. numProcs - 1) then
     localEnd = n
@@ -23,16 +34,19 @@ PROGRAM prac2
     localEnd = 2 + myRank * k + k - 1
   end if
   allocate(arr(localEnd - localStart + 1))
-  
   do k = localStart, localEnd
     arr(k - localStart + 1) = k
   end do
 
   do k = 2, int(sqrt(real(n)))
-    ! TODO is there a memory leak here ? valgrind result: no memory leak
+    ! eliminate multiples of k
+    ! TODO memory(new array is smaller than initially allocated) valgrind result: no memory leak
     arr = pack(arr,mod(arr,k)/=0)
   end do
 
+  !print*, "Process ", myRank, ": ", arr
+
+  !PRINT THE MAX
   !we cannot send last element in the array from the last process only
   !because last process might have an empty array(in fact we can't know which is
   !the first process from the last one in descending order which have a non
@@ -44,8 +58,8 @@ PROGRAM prac2
    else
       k = arr(size(arr))
    end if  
-   call mpi_reduce(k, maxValue, 1, MPI_INTEGER, MPI_MAX, master,  MPI_COMM_WORLD, ierr)   
   deallocate(arr)
+   call mpi_reduce(k, maxValue, 1, MPI_INTEGER, MPI_MAX, master,  MPI_COMM_WORLD, ierr)   
   if(myRank == master) then
     print*, "max = ", maxValue
   end if
